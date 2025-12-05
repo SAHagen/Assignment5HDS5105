1b6930bffba20782b12bb66b66e6d68c4183d3d2
SAHagen <s.hagen1@universityofgalway.ie>
2025-12-05 15:24:46 +0000

    Bux fixing

--- server/regressionServer.R
+++ server/regressionServer.R
@@ -5,14 +5,14 @@ regressionServer <- function(id) {
       num_data <- dig_data%>%
         select(all_of(input$num_var))
       columnnames = colnames(num_data)
-      print(summary(lm))
-      if(ncol(num_data) == 2){
-        lm = lm(num_data[,2] ~ num_data[,1])
-        plot(x = num_data[,1], y = num_data[,2], 
-             xlab = columnnames[1],
-             ylab = columnnames[2]) +
-          abline(lm, col = "red")
+      if(ncol(num_data) != 2){
+       return() 
       }
+      lm = lm(num_data[,2] ~ num_data[,1])
+      plot(x = num_data[,1], y = num_data[,2], 
+          xlab = columnnames[1],
+          ylab = columnnames[2]) +
+          abline(lm, col = "red")
     })
   })
 }
